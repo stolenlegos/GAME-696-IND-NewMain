959cef05cc85dbc3c4380123121f4f0e2295d11e
stolenlegos <stolenlegos@gmail.com>
2021-12-11 15:20:14 -0500

    candy essence

--- Barebones_Project/Assets/Scripts/RitualEventsObserver.cs
+++ Barebones_Project/Assets/Scripts/RitualEventsObserver.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System; 
+using System;
 
 public class RitualEventsObserver : MonoBehaviour
 {
@@ -12,153 +12,153 @@ public class RitualEventsObserver : MonoBehaviour
     [SerializeField]
     private int numActiveAshes;
     [SerializeField]
-    private int numCandiesInBowl;  
+    private int numCandiesInBowl;
     private bool circleActive;
     [SerializeField]
     private List<GameObject> ghostOrbs;
     [SerializeField]
-    private List<GhostObject> _ghosts; 
+    private List<GhostObject> _ghosts;
     public GameObject currentGhostState;
-    private bool _halfGhost; 
+    private bool _halfGhost;
     private bool _fullGhost;
+    [SerializeField] private GameObject candyEssence;
 
     public static event Action ActivateCircle;
-    private bool _bowlFilledComplete;  
+    private bool _bowlFilledComplete;
     private bool _magicSageCircleStateComplete;
-    private bool _candlesStateComplete; 
-    private bool _elementalSpotsStateComplete;  
-    
-    private void Start() { 
+    private bool _candlesStateComplete;
+    private bool _elementalSpotsStateComplete;
+
+    private void Start() {
         _magicSageCircleStateComplete = false;
         _candlesStateComplete = false;
         _elementalSpotsStateComplete = false;
         _ghosts = new List<GhostObject>();
         _halfGhost = false;
-        _fullGhost = false; 
+        _fullGhost = false;
         GhostObject undecided = new GhostObject(ghostOrbs[0], ghostOrbs[1], "Undecided", "N/A", false);
         GhostObject ian = new GhostObject(ghostOrbs[2], ghostOrbs[3], "Ian", "Photo", false );
         GhostObject aunt = new GhostObject(ghostOrbs[4], ghostOrbs[5], "Aunt", "Comb", false);
         GhostObject chuck = new GhostObject(ghostOrbs[6], ghostOrbs[7], "Chuck","Knife", false);
         _ghosts.Add(undecided);
         _ghosts.Add(ian);
-        _ghosts.Add(aunt); 
-        _ghosts.Add(chuck);  
+        _ghosts.Add(aunt);
+        _ghosts.Add(chuck);
         SetCurrentGhostSet();
     }
 
     public static event Action BlowOutCandles;
-    public static event Action NegateGlowingSpots;  
-    private void Update() { 
-        if (numCandiesInBowl == 4) { 
-            _bowlFilledComplete = true; 
+    public static event Action NegateGlowingSpots;
+    private void Update() {
+        if (numCandiesInBowl == 4 && _magicSageCircleStateComplete) {
+            _bowlFilledComplete = true;
+            candyEssence.SetActive(true);
         }
         if (numActiveAshes == 8) {
             _magicSageCircleStateComplete = true;
-            ActivateCircle?.Invoke();  
+            ActivateCircle?.Invoke();
         }
         if (numActiveElems == 4 && _magicSageCircleStateComplete) {
-            _elementalSpotsStateComplete = true; 
+            _elementalSpotsStateComplete = true;
         } else if (numActiveElems == 4 && !_magicSageCircleStateComplete) {
-            NegateGlowingSpots?.Invoke(); 
+            NegateGlowingSpots?.Invoke();
         }
         if (numActiveCandles == 4 && _magicSageCircleStateComplete) {
-            _halfGhost = true; 
+            _halfGhost = true;
             SetCurrentGhostSet();
-            _candlesStateComplete = true;   
-        } else if (numActiveCandles == 4 && !_magicSageCircleStateComplete) { 
-            BlowOutCandles?.Invoke(); 
+            _candlesStateComplete = true;
+        } else if (numActiveCandles == 4 && !_magicSageCircleStateComplete) {
+            BlowOutCandles?.Invoke();
         }
         if (_candlesStateComplete && _elementalSpotsStateComplete
         && _bowlFilledComplete && _magicSageCircleStateComplete) {
             _halfGhost = false;
-            _fullGhost = true;  
-            SetCurrentGhostSet(); 
+            _fullGhost = true;
+            SetCurrentGhostSet();
         }
     }
 
     private void OnEnable() {
         RitualScripts.IncrementElem += IncrementCount;
         MatchLight.IncrementCandle += IncrementCount;
-        AshStepEvent.IncrementAshes += IncrementCount; 
+        AshStepEvent.IncrementAshes += IncrementCount;
         GhostObjDetectTable.ToggleBool += ToggleGhostObjectBool;
-        Bowl.IncrementCandy += IncrementCount;    
+        Bowl.IncrementCandy += IncrementCount;
     }
 
     private void OnDisable() {
         RitualScripts.IncrementElem -= IncrementCount;
         MatchLight.IncrementCandle -= IncrementCount;
-        AshStepEvent.IncrementAshes -= IncrementCount;   
-        GhostObjDetectTable.ToggleBool -= ToggleGhostObjectBool;  
-        Bowl.IncrementCandy -= IncrementCount;   
+        AshStepEvent.IncrementAshes -= IncrementCount;
+        GhostObjDetectTable.ToggleBool -= ToggleGhostObjectBool;
+        Bowl.IncrementCandy -= IncrementCount;
 
     }
-    
+
     private void IncrementCount(int num, string itemType) {
         if (itemType == "element") {
-            numActiveElems += num; 
+            numActiveElems += num;
         }
         if (itemType == "candle") {
             numActiveCandles += num;
         }
         if (itemType == "ashes") {
-            numActiveAshes += num; 
+            numActiveAshes += num;
         }
         if (itemType == "candy") {
-            numCandiesInBowl += num; 
+            numCandiesInBowl += num;
         }
     }
 
     private void ToggleGhostObjectBool(bool ghostState, string ghostItemName) {
         foreach (GhostObject ghost in _ghosts) {
             if (ghost.ghostObject == ghostItemName) {
-                ghost.ghostActive = ghostState; 
-                return; 
+                ghost.ghostActive = ghostState;
+                return;
             }
         }
-        SetCurrentGhostSet(); 
+        SetCurrentGhostSet();
     }
-    
+
     private void SetCurrentGhostSet() {
-        int numVoicesActive = 0; 
+        int numVoicesActive = 0;
         foreach (GhostObject ghost in _ghosts) {
             if (ghost.ghostActive) {
                 numVoicesActive++;
-            } 
+            }
         }
         if (numVoicesActive != 1) {
             if ((currentGhostState.name == _ghosts[0].halfGhost.name && _halfGhost) ||
-                (currentGhostState.name == _ghosts[0].fullGhost.name && _fullGhost)) {   
+                (currentGhostState.name == _ghosts[0].fullGhost.name && _fullGhost)) {
                 return;
             } else {
                 if (_halfGhost)
                     SwapGhost(_ghosts[0].halfGhost);
                 if (_fullGhost)
                     SwapGhost(_ghosts[0].fullGhost);
-                return; 
+                return;
             }
         } else {
-            for (int i = 0; i < _ghosts.Count; i++) { 
+            for (int i = 0; i < _ghosts.Count; i++) {
                 if (_ghosts[i].ghostActive) {
                     if ((currentGhostState.name == _ghosts[i].halfGhost.name && _halfGhost) ||
-                        (currentGhostState.name == _ghosts[i].fullGhost.name && _fullGhost)) {   
+                        (currentGhostState.name == _ghosts[i].fullGhost.name && _fullGhost)) {
                         return;
                     } else {
                         if (_halfGhost)
                             SwapGhost(_ghosts[i].halfGhost);
                         if (_fullGhost)
                             SwapGhost(_ghosts[i].fullGhost);
-                        return; 
+                        return;
                     }
                 }
-            } 
+            }
         }
     }
 
-    private void SwapGhost(GameObject newGhost) { 
-            currentGhostState.SetActive(false); 
+    private void SwapGhost(GameObject newGhost) {
+            currentGhostState.SetActive(false);
             currentGhostState = newGhost;
-            currentGhostState.SetActive(true); 
+            currentGhostState.SetActive(true);
     }
 }
-
-
